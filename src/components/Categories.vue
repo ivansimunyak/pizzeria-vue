e1ff05e41163c3fd992957099af676c8372b1600
Ivan <simunyakivan@gmail.com>
2021-12-28 23:53:39 +0100

    categories done, remake table, added :key

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,34 +1,23 @@
 <template>
-    <div id="wrapper">
 <h1 id="categoriesHeader">Categories</h1>
-
-    <table class="table">
-    
-     <thead>
-          <tr>
-              <th>{{header}}</th>
-             <th>Edit</th>
-              <th>Remove</th>
-          </tr>
-     </thead>
-      <tbody>
-          <tr v-for="(category, index) in categories" :key="index">
-              <td>{{category[column]}}</td>
-                 <td><btn-styled class="btnEdit" @click="editCategory(category.id,category.name,index)">Edit</btn-styled></td>
-                 <td><btn-styled class="btnDelete" @click="removeCategory(category.id,index)">Remove</btn-styled></td>
-          </tr>   
-      </tbody>
+<section class="categoriesTable">
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Edit</th>
+                <th>Remove</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(category, index) in categories" :key="index">
+                <td>{{category.name}}</td>
+                <td><btn-styled class="btnEdit" @click="editCategory(category.id,category.name,index)">Edit</btn-styled> </td>
+                <td><btn-styled class="btnDelete" @click="removeCategory(category.id,index)">Remove</btn-styled> </td>
+            </tr>
+        </tbody>
     </table>
-        <h2 id="addNew">Add new category</h2>
-    <div class="wrapForm">
-     <form class="form" @submit.prevent="submitForm">
-  <label for="fname">Name:</label><br>
-  <input type="text" ref="inputName" id="name" name="name" required v-model="name"><br>
- <btn-styled type="submit">Submit</btn-styled>
-</form>
-
-
-</div>
+</section>
 <h2 id="editCategory">Edit category</h2>
 <div class="edit">
      <form class="form" @submit.prevent="editCategoryForm">
@@ -39,8 +28,6 @@
  <btn-styled type="submit">Submit</btn-styled>
 </form>
 </div>
-    </div>
-    
 </template>
 <script>
 import axios from 'axios'
@@ -50,36 +37,18 @@ export default {
     data(){
         return{
               categories:[],
-            header:'Name',
-            column:'name',
-            name:'',
-            latestRecord:'',
+            column:'name',         
             editName:'',
             editID:'',
-            objecttest:[{name:'',id:'',index:''}],
-            saveIndex:'',
-            idArray:[]
+            saveIndex:'',    
         }
     }, mounted(){
     const url='http://localhost:3000/api/productCategory/';
      axios.get(url).then((response) =>{
           this.categories = response.data;
-          this.latestRecord=this.categories.slice(-1);
       } );
     },methods:{
-                submitForm(){
-            axios.post('http://localhost:3000/api/productCategory/addcategory', {name:this.name})
-                 .then((res) => {
-                     //Perform Success Action
-                     console.log(res.data);  
-                     this.rerunData(this.name);
-                 })
-                 .catch((error) => {
-                     // error.response.status Check status code
-                     console.log( error.response.status)
-                 });
-              
-        },removeCategory(id,index){
+                removeCategory(id,index){
             axios.post('http://localhost:3000/api/productCategory/removecategory/'+id)
                  .then((res) => {
                      //Perform Success Action
@@ -111,14 +80,6 @@ export default {
             this.editID=id;
         // this.objecttest={name:name,id:id,index:index};
         this.saveIndex=index;
-        },
-        rerunData(name){
-             const url='http://localhost:3000/api/productCategory/';
-         axios.get(url).then((response) =>{
-          this.idArray=response.data;
-          var lastID=this.idArray[this.idArray.length-1].id;
-          this.categories.push({name:name,id:lastID})
-      } );
         }
     }
     
@@ -126,17 +87,7 @@ export default {
 </script>
 
 <style scoped>
-#wrapper{
-   background-color: #ffffffd9;
-    position:absolute;
-    width:80%;
-    height:100%;
-    margin: 0px;
-    top: 10%;
-    margin-left: 10%;
-    margin-right: 10%;
-    border-style: outset;
-}
+
 #categoriesHeader{
     position: absolute;
 font-family: 'Times New Roman', Times, serif;
@@ -144,66 +95,6 @@ font-size: 45px;
 width: 30%;
 left: 30%;
 top: 1%; 
-}
-.wrapForm{
-font-family: 'Times New Roman', Times, serif;
-color: black;
-border-style:groove;
-width: 30%;
- background-color: rgb(255, 255, 255);
- position:relative;
- left: 50%;
- top: 20%;
-}
-table {
-    border-collapse: collapse;
-    table-layout: fixed;
-    margin: 25px 0;
-    position:relative;
-    font-size: 0.9em;
-    padding: 0;
-    font-family: sans-serif;
-    min-width: 400px;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
-      font-family: -apple-system,system-ui,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji";
-     text-align: center;
-       margin-left:5%; 
-    margin-right:auto;
-    position:absolute;
-    top:20%;
-    
-}
-tbody tr {
-    background-color: #f8f8f8;
-    border: 1px solid #ddd;
-    padding: .25em;
-    
-    
-}
-tbody td{
-     font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;
-}
-thead tr{
-     background-color: #a80000;
-    color: #ffffff;
-    text-align: left;
-      font-weight: bold;
-}
-
-table th,
-table td {
-    width: 15px ;
-    padding: 12px 15px;
-    text-align: center;
-    
-}
-
-table th {
-    
-    font-size: 12.5px;
-    letter-spacing: .1em;
-    
-    
 }
 .form{
    font-family:"Bookerly";
@@ -216,14 +107,24 @@ input[type=text] {
   box-shadow: 5px;
   border-width:1px;
 }
-#addNew{
-    position: relative;
-    left:150px;
-    top:20%;
+input[type=number] {
+  padding:5px;
+  margin:5px 0;
+  border-radius:10px;
+  box-shadow: 5px;
+  border-width:1px;
+}
+
+#editCategory{
+    position: absolute;
+    top: 45%;
+    width: 30%;
+    margin-left: 25%;
+    left: 25%;
 }
 .edit{
-    position: relative;
-    top: 20%;
+    position: absolute;
+    top: 55%;
     left:50%;
     font-family: 'Times New Roman', Times, serif;
 color: black;
@@ -231,11 +132,35 @@ border-style:groove;
 width: 30%;
  background-color: rgb(255, 255, 255);
 }
-#editCategory{
+.categoriesTable{
+      border: 1px solid #999;
+    border-radius: 1px;
+    color: #333;
+    background: white;
+    overflow: auto;
+    height: 250px;
+    width: 40%;
     position: relative;
-    top: 20%;
-    width: 30%;
-    margin-left: 25%;
-    left: 25%;
+    top:40%;
+    margin-left: 2.5%;
 }
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+         th {
+            position: sticky;
+            top: 0;
+            background: #a80000;
+            padding: 10px 5px;
+            text-align: center;
+            border-bottom: 1px solid #a80000;
+            color: white;
+            z-index: 3;
+        }
+               td {
+            padding: 5px 5px;
+            text-align: center;
+            z-index: 1;
+        }
 </style>
--- src/views/CategoriesPage.vue
+++ src/views/CategoriesPage.vue
@@ -1,12 +1,85 @@
 <template>
-    <categories></categories>
+
+<div id="wrapper">
+    <categories :key="this.uniqueCatKey"></categories>
+         <h2 id="addNew">Add new category</h2>
+    <div class="wrapForm">
+     <form class="form" @submit.prevent="submitForm">
+  <label for="fname">Name:</label><br>
+  <input type="text" ref="inputName" id="name" name="name" required v-model="addingName"><br>
+ <btn-styled type="submit">Submit</btn-styled>
+</form>
+</div>
+ </div>
 </template>
 <script>
+import BtnStyled from "../components/BtnStyled.vue"
 import Categories from "../components/Categories.vue"
+import axios from 'axios'
 export default {
     components:{
+        BtnStyled,
         Categories
+    },data(){
+        return{
+            uniqueCatKey:0,
+            addingName:''
+
+        }
+    },
+    methods:{
+        submitForm(){
+            axios.post('http://localhost:3000/api/productCategory/addcategory', {name:this.addingName})
+                 .then((res) => {
+                     //Perform Success Action
+                     console.log(res.data);  
+                    this.uniqueCatKey++;
+                    this.addingName='';
+                
+                 })
+                 .catch((error) => {
+                     // error.response.status Check status code
+                     console.log( error.response.status)
+                 });
+                
+        }
     }
     
 }
 </script>
+<style scoped>
+#wrapper{
+   background-color: #ffffffd9;
+    position:absolute;
+    width:80%;
+    height:100%;
+    margin: 0px;
+    top: 10%;
+    margin-left: 10%;
+    margin-right: 10%;
+    border-style: outset;
+}
+
+.wrapForm{
+font-family:"Bookerly";
+color: black;
+border-style:groove;
+width: 30%;
+ background-color: rgb(255, 255, 255);
+ position:absolute;
+ left: 50%;
+ top: 30%;
+}
+#addNew{
+    position: absolute;
+    left: 55%;
+    top:19%;
+}
+input[type=text] {
+  padding:5px;
+  margin:5px 0;
+  border-radius:10px;
+  box-shadow: 5px;
+  border-width:1px;
+}
+</style>
